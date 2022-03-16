307da0db2b4aa762244371fca2b7adb094f3d167
Gekyume6666 <101752049+Gekyume6666@users.noreply.github.com>
2022-03-16 17:07:37 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-        TOKEN: '', //write your discord bot token
+        TOKEN: 'OTUzNzQzODAzMjU1MjQyODc1.YjJBIg.I4RgyzxDTjNyYg6kUDnD0hXSpqo', //write your discord bot token
         px: '!',
         playing: 'Umut Bayraktar ❤️ Music Bot',
 
